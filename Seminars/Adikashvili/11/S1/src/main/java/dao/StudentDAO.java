dc1ebfad922927e9f21ae5e6efbdbee2fc4024a7
adikashvili <v-giorgiad@microsoft.com>
2025-05-13 12:46:03 +0400

    wrong code

--- Seminars/Adikashvili/11/S1/src/main/java/dao/StudentDAO.java
+++ Seminars/Adikashvili/11/S1/src/main/java/dao/StudentDAO.java
@@ -10,7 +10,7 @@ import java.util.List;
 import static util.Converter.toCourse;
 import static util.Converter.toStudent;
 
-public class StudentDAO {
+publicc class StudentDAO {
 
     private final Connection connection;
 
